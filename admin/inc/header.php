892f70be4c75be662f8170c639692e839d80afa4
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2024-04-25 23:43:31 +0600

    Dashboard customized part done

---
+++ admin/inc/header.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en" class="semi-dark">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php include "inc/css.php" ?>
+	<title>Dashboard | Home</title>
+</head>
+
+<body>
---
+++ admin/inc/leftmenu.php
@@ -0,0 +1,57 @@
+		<!-- ########## START: SIDE BAR ########## -->
+		<!--sidebar wrapper -->
+		<div class="sidebar-wrapper" data-simplebar="true">
+			<div class="sidebar-header">
+				<div>
+					<img src="assets/images/logo-icon.png" class="logo-icon" alt="logo icon">
+				</div>
+				<div>
+					<h4 class="logo-text">Admin Panel</h4>
+				</div>
+				<div class="toggle-icon ms-auto"><i class='bx bx-arrow-to-left'></i>
+				</div>
+			</div>
+			<!--navigation-->
+			<ul class="metismenu" id="menu">
+				<li>
+					<a href="dashboard.php">
+						<div class="parent-icon"><i class='bx bx-home-circle'></i>
+						</div>
+						<div class="menu-title">Dashboard</div>
+					</a>
+				</li>
+
+				<!-- ########## START: CATEGORY MANAGEMENT ########## -->
+				<li class="menu-label">CATEGORY MANAGEMENT</li>
+				<li>
+					<a href="javascript:;" class="has-arrow">
+						<div class="parent-icon"><i class='bx bx-category'></i>
+						</div>
+						<div class="menu-title">Category</div>
+					</a>
+					<ul>
+						<li> <a href="ecommerce-products.html"><i class="bx bx-radio-circle"></i>Manage All Category</a>
+						</li>
+						<li> <a href="ecommerce-add-new-products.html"><i class="bx bx-radio-circle"></i>Add New Category</a>
+						</li>
+						<li> <a href="ecommerce-orders.html"><i class="bx bx-radio-circle"></i>Trash</a>
+						</li>
+					</ul>
+				</li>
+				<!-- ########## END: CATEGORY MANAGEMENT ########## -->
+
+
+			
+				<li class="menu-label">Others</li>
+				<li>
+					<a href="" target="_blank">
+						<div class="parent-icon"><i class="bx bx-support"></i>
+						</div>
+						<div class="menu-title">Support</div>
+					</a>
+				</li>
+			</ul>
+			<!--end navigation-->
+		</div>
+		<!--end sidebar wrapper -->
+		<!-- ########## START: SIDE BAR ########## -->
